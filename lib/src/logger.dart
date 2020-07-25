ce80f665e48f30f42b1e65b7a39927434657476d
aswinmurali-io <aswinmurali.co@gmail.com>
2020-07-25 20:48:31 +0530

    add an experimental logger system

--- lib/src/logger.dart
+++ lib/src/logger.dart
@@ -1,12 +1,42 @@
+import 'dart:mirrors';
+
 import 'package:logger/logger.dart';
 
-abstract class Loggable {
-  String get label;
+class CustomLogFilter extends LogFilter {
+  @override
+  bool shouldLog(LogEvent event) => true;
+}
 
-  final _logger = Logger(printer: PrettyPrinter(printTime: true));
+class CustomLogPrinter extends LogPrinter {
+  final dynamic classname;
+  CustomLogPrinter(this.classname);
 
-  void log(Level level, message, [error, StackTrace stackTrace]) {
-    _logger.log(level, label);
-    _logger.log(level, message);
+  @override
+  List<String> log(LogEvent event) {
+    var name = reflect(classname).reflectee.toString();
+    return [
+      '[${DateTime.now().toString().substring(0, 19)}] Recipe of ${reflect(name).reflectee.toString().substring(13, name.length - 1)} | ${event.message}'
+    ];
   }
+}
+
+class Loggable {
+  Logger log;
+  void init(dynamic classname) {
+    log = Logger(
+      level: Level.verbose,
+      filter: CustomLogFilter(),
+      printer: CustomLogPrinter(classname),
+    );
+  }
+}
+
+class Recipe extends Loggable {}
+
+class CoffeCup extends Recipe {}
+
+void main() {
+  Recipe recipe = CoffeCup();
+  recipe.init(recipe);
+  recipe.log.d('hi');
 }
